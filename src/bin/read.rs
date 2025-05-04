f7d9322dbf1b2dc0930d691f03a7ae6c4ec7852f
ryokino <ryo.kinoshita212@gmail.com>
2025-05-04 19:37:08 +0900

    標準出力と標準入力の処理を追加

--- src/bin/read.rs
+++ src/bin/read.rs
@@ -1,12 +1,21 @@
-use std::io::{self, BufRead};
+use std::io::{self, BufRead}; // inputを読み込むためのモジュール。BufReadを使うと1行ずつ読みやすい
 
 fn main() {
-    let stdin = io::stdin();
+    let stdin = io::stdin(); // selfを使っているからioを使っていい。こうすることで、Rustの標準入力用の入り口(ハンドル)を取得できる
+                             // ハンドルはグローバルな共有リソースに向いている。プロセスに一つしか標準出力はないけど複数のスレッドがあるからね。
+                             // mutex: 一人ずつしか使えないようにする。 stdinはmutexの制限があるのでちゃんと .lock() を使ってね
+                             // ロックを取得すると、スレッドがブロックされて、競合が起きない
+
     let mut buffer = String::new();
 
     // 標準入力から1行読み込む
     stdin.lock().read_line(&mut buffer).unwrap();
+    // read_lineは改行までの文字列を読む
 
-    let food_str = buffer.trim(); // 改行を除去
+    let food_str = buffer.trim(); // 末尾の改行を除去
+                                  // trim は String(可変)の静的メソッドで、先頭と末尾の空白文字を除去する
+                                  // 返り値は &str 型=Stringのスライス = 一時的に表示しているだけ
+                                  // &strにすることで安全に読み取り専用にすることができる。
+
     println!("I like {}!", food_str);
 }
